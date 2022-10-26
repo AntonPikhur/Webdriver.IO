4c61cfbe64dfa845045a1e2f9235758d4bcd555d
AntonPikhur <113526199+AntonPikhur@users.noreply.github.com>
2022-10-26 14:23:49 +0300

    update

--- README.md
+++ README.md
@@ -2,5 +2,4 @@ npm init
 npm init wdio
 npm install @wdio/allure-reporter --save-dev
 npm i allure-commandline
-npm install @wdio/selenium-standalone-service --save-dev ?
-npm install @wdio/firefox-profile-service --save-dev
+npm install @wdio/selenium-standalone-service --save-dev
--- test/specs/mainPage.test.js
+++ test/specs/mainPage.test.js
@@ -9,75 +9,75 @@ describe('Testing of Telnyx main page using Webdriver.IO', () => {
         await mainPage.callUsClick();
         await expect(mainPage.callUsModal).toBeDisplayed();
     })
-    it('2. Should click on "Support Center" and transfer to support page', async() => {
+    xit('2. Should click on "Support Center" and transfer to support page', async() => {
         await mainPage.supportCntrClick();
         await expect(browser).toHaveUrl('https://support.telnyx.com/en/');
     })
-    it('3. Should click on "Log in" and transfer to log in page', async() => {
+    xit('3. Should click on "Log in" and transfer to log in page', async() => {
         await mainPage.loginClick();
         await expect(browser).toHaveUrlContaining('/login/sign-in');
     })
-    it('4. Should click on "Sign Up" and transfer to sign up page', async() =>{
+    xit('4. Should click on "Sign Up" and transfer to sign up page', async() =>{
         await mainPage.signUpClick();
         await expect(browser).toHaveUrl('https://telnyx.com/sign-up');
     })
-    it('5. Should check if Customer logo of Cisco is displayed', async() => {
+    xit('5. Should check if Customer logo of Cisco is displayed', async() => {
         await mainPage.cLogoScroll();
         await mainPage.cLogo1.isDisplayedInViewport();
     })
-    it('6. Should check if Customer logo of Philips is displayed', async() => {
+    xit('6. Should check if Customer logo of Philips is displayed', async() => {
         await mainPage.cLogoScroll();
         await mainPage.cLogo2.isDisplayedInViewport();
     })
-    it('7. Should check if Customer logo of HP is displayed', async() => {
+    xit('7. Should check if Customer logo of HP is displayed', async() => {
         await mainPage.cLogoScroll();
         await mainPage.cLogo3.isDisplayedInViewport();
     })
-    it('8. Should check if Customer logo of American Red Cross is displayed', async() => {
+    xit('8. Should check if Customer logo of American Red Cross is displayed', async() => {
         await mainPage.cLogoScroll();
         await mainPage.cLogo4.isDisplayedInViewport();
     })
-    it('9. Should check if Customer logo of Talkdesk is displayed', async() => {
+    xit('9. Should check if Customer logo of Talkdesk is displayed', async() => {
         await mainPage.cLogoScroll();
         await mainPage.cLogo5.isDisplayedInViewport();
     })
-    it('10. Should click on "Voice API" button and transfer to correct page', async() => {
+    xit('10. Should click on "Voice API" button and transfer to correct page', async() => {
         await mainPage.voiceApiClick();
         await expect(browser).toHaveUrl('https://telnyx.com/products/voice-api');
     })
-    it('11. Should click on "Elastic SIP Trunking" button and transfer to correct page', async() => {
+    xit('11. Should click on "Elastic SIP Trunking" button and transfer to correct page', async() => {
         await mainPage.sipTrunkClick();
         await expect(browser).toHaveUrl('https://telnyx.com/products/sip-trunks');
     })
-    it('12. Should click on "Messaging API" button and transfer to correct page', async() => {
+    xit('12. Should click on "Messaging API" button and transfer to correct page', async() => {
         await mainPage.messApiClick();
         await expect(browser).toHaveUrl('https://telnyx.com/products/sms-api');
     })
-    it('13. Should сlick on "International Numbers" button and transfer to correct page', async() => {
+    xit('13. Should сlick on "International Numbers" button and transfer to correct page', async() => {
         await mainPage.intNumbrsClick();
         await expect(browser).toHaveUrl('https://telnyx.com/use-cases/international-numbers');
     })
-    it('14. Should click on "Explore Video API" button and transfer to correct page', async() => {
+    xit('14. Should click on "Explore Video API" button and transfer to correct page', async() => {
         await mainPage.vidApiClick();
         await expect(browser).toHaveUrl('https://telnyx.com/products/video-api');
     })
-    it('15. Should сlick on "Wireless" button transfer to correct page', async() => {
+    xit('15. Should сlick on "Wireless" button transfer to correct page', async() => {
         await mainPage.wirelessClick();
         await expect(browser).toHaveUrl('https://telnyx.com/products/iot-sim-card');
     })
-    it('16. Should check if Best Result award is displayed', async() => {
+    xit('16. Should check if Best Result award is displayed', async() => {
         await mainPage.rewordsScroll();
         await mainPage.reword1.isDisplayedInViewport();
     })
-    it('17. Should check if Best Usability award is displayed', async() => {
+    xit('17. Should check if Best Usability award is displayed', async() => {
         await mainPage.rewordsScroll();
         await mainPage.reword2.isDisplayedInViewport();
     })
-    it('18. Should check if Leader award is displayed', async() => {
+    xit('18. Should check if Leader award is displayed', async() => {
         await mainPage.rewordsScroll();
         await mainPage.reword3.isDisplayedInViewport();
     })
-    it('19. Should check if Momentum Leader award is displayed', async() => {
+    xit('19. Should check if Momentum Leader award is displayed', async() => {
         await mainPage.rewordsScroll();
         await mainPage.reword4.isDisplayedInViewport();
     })
